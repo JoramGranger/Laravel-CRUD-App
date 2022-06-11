01161b50eab7f5bd5bf466f8e1d421e3f9a536f6
JORAMGRANGER <joramgranger@outlook.com>
2022-06-11 01:30:17 -0700

    commit 1

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>Welcome</h1>
+        <p>this is lsapp</p>
+        <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+            <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
+    </div>
+@endsection
